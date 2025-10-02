af114f783a91fa7a108e8890c326fe8ff7f04eb4
floramos06 <202200303@est.umss.edu>
2025-10-02 15:13:23 -0400

    Atomo subtitulo creado

---
+++ componentes/atomos/subtitulo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, TextStyle } from 'react-native';
+
+interface SubtituloProps {
+    children: React.ReactNode;
+    style?: TextStyle;
+}
+
+const Subtitulo: React.FC<SubtituloProps> = ({ children, style}) => {
+    return (
+       <Text style={[styles.text, style]}>
+        {children}
+       </Text> 
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 11,
+        color: "#000000",
+        fontWeight: "light",
+        fontFamily: "Almarai",
+    },
+});
+
+export default Subtitulo;
