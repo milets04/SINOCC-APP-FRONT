52f88412b5f9a49c94ab9d93c14b32301a35f9a9
Milena <miletorrico0041@gmail.com>
2025-10-01 23:22:00 -0400

    restauracion index

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
 import { Text, View } from "react-native";
-import MenuIcon from '@/componentes/atomos/menu';
-import { Ionicons } from '@expo/vector-icons';
 
-const handleMapPress = () => {
-    console.log('Mapa presionado');
-    // Tu lógica aquí
-  };
 export default function Index() {
   return (
     <View
@@ -21,11 +15,6 @@ export default function Index() {
         fontSize: 20,
         textAlign: "center"
       }}>Sistema de Notificacion de Cierre de Calles - SINOCC</Text>
-
-      <MenuIcon
-        icon={<Ionicons name="map-outline" size={32} color="#146BF6" />}
-        onPress={handleMapPress}
-      />
     </View>
   );
 }
