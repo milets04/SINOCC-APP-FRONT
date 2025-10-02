af59f9ec13e2ef697c48ff22145fc19cf57cf962
Milena <miletorrico0041@gmail.com>
2025-10-01 23:21:28 -0400

    prueba funcionamiento menu

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,11 @@
 import { Text, View } from "react-native";
+import MenuIcon from '@/componentes/atomos/menu';
+import { Ionicons } from '@expo/vector-icons';
+
+const handleMapPress = () => {
+    console.log('Mapa presionado');
+    // Tu lógica aquí
+  };
 export default function Index() {
   return (
     <View
@@ -14,6 +21,11 @@ export default function Index() {
         fontSize: 20,
         textAlign: "center"
       }}>Sistema de Notificacion de Cierre de Calles - SINOCC</Text>
+
+      <MenuIcon
+        icon={<Ionicons name="map-outline" size={32} color="#146BF6" />}
+        onPress={handleMapPress}
+      />
     </View>
   );
 }
