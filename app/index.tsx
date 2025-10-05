d23c316e8b1625221c27df5ebcf7b48d32eff15f
Milena <miletorrico0041@gmail.com>
2025-10-05 14:13:41 -0400

    Menu Inferior HomePage

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,14 @@
+import MenuInf from '@/componentes/moleculas/menuInf';
+import { Ionicons } from '@expo/vector-icons';
 import { Text, View } from "react-native";
+
+const handleGoHome = () => {
+    console.log('Navegando a Home');
+  };
+
+  const handleGoMap = () => {
+    console.log('Navegando a Mapa');
+  };
 
 export default function Index() {
   return (
@@ -15,6 +25,13 @@ export default function Index() {
         fontSize: 20,
         textAlign: "center"
       }}>Sistema de Notificación de Cierre de Calles</Text>
+
+      <MenuInf
+        homeIcon={<Ionicons name="home-outline" size={32} color="#146BF6" />}
+        mapIcon={<Ionicons name="map-outline" size={32} color="#146BF6" />}
+        onHomePress={handleGoHome}
+        onMapPress={handleGoMap}
+      />
     </View>
   );
 }
