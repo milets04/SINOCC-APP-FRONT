f41dc355c4290add31c922fac9fa0dc2b710d2ec
Milena <miletorrico0041@gmail.com>
2025-10-08 21:48:20 -0400

    cambios

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,4 @@
-import MenuInf from '@/componentes/moleculas/menuInf';
-import { Ionicons } from '@expo/vector-icons';
 import { Text, View } from "react-native";
-
-const handleGoHome = () => {
-    console.log('Navegando a Home');
-  };
-
-  const handleGoMap = () => {
-    console.log('Navegando a Mapa');
-  };
 
 export default function Index() {
   return (
@@ -25,13 +15,6 @@ export default function Index() {
         fontSize: 20,
         textAlign: "center"
       }}>Sistema de Notificación de Cierre de Calles</Text>
-
-      <MenuInf
-        homeIcon={<Ionicons name="home-outline" size={32} color="#146BF6" />}
-        mapIcon={<Ionicons name="map-outline" size={32} color="#146BF6" />}
-        onHomePress={handleGoHome}
-        onMapPress={handleGoMap}
-      />
     </View>
   );
 }
