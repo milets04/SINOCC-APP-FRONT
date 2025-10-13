14cb51c34464269c624403a259c6e065761f5162
Milena <miletorrico0041@gmail.com>
2025-10-13 00:14:36 -0400

    Pantalla Notificaciones estática terminada

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,9 @@
-import ZoneNotifications from '@/componentes/moleculas/menuZonas';
-import { useState } from 'react';
-import { Text, View } from "react-native";
-export default function Index() {
-  const [zones, setZones] = useState([
-    { id: '1', name: 'Sacaba', enabled: false },
-    { id: '2', name: 'Quillacollo', enabled: false },
-    { id: '3', name: 'Zona centro', enabled: false },
-  ]);
+import PantallaNotificaciones from '@/componentes/templates/pantallaNotif';
 
-  const handleZoneToggle = (zoneId: string, newValue: boolean) => {
-    setZones(prevZones =>
-      prevZones.map(zone =>
-        zone.id === zoneId ? { ...zone, enabled: newValue } : zone
-      )
-    );
-    console.log(`Zona ${zoneId} cambió a: ${newValue}`);
-  };
+export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{
-        color: "blue",
-        fontWeight: "bold",
-        fontSize: 20,
-        textAlign: "center"
-      }}>Sistema de Notificación de Cierre de Calles</Text>
-      <ZoneNotifications
-        zones={zones}
-        onZoneToggle={handleZoneToggle}
-      />
-    </View>
+
+      <PantallaNotificaciones />
+
   );
 }
---
+++ componentes/templates/pantallaNotif.tsx
@@ -0,0 +1,63 @@
+import Header from '@/componentes/moleculas/header';
+import MenuInf from '@/componentes/moleculas/menuInf';
+import NotificationCard from '@/componentes/moleculas/notificacion';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+export default function pantallaNotificaciones() {
+  const handleGoHome = () => {
+    console.log('Navegando a Home');
+  };
+
+  const handleGoMap = () => {
+    console.log('Navegando a Mapa');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <ScrollView 
+        style={styles.scrollContent}
+        contentContainerStyle={styles.scrollContentContainer}
+      >
+        <NotificationCard
+          color="red"
+          title="Title"
+          description="Description"
+        />
+        <NotificationCard
+          color="green"
+          title="Title"
+          description="Description"
+        />
+        <NotificationCard
+          color="yellow"
+          title="Title"
+          description="Description"
+        />
+      </ScrollView>
+
+      <MenuInf
+        homeIcon={<Ionicons name="home-outline" size={32} color="#146BF6" />}
+        mapIcon={<Ionicons name="map-outline" size={32} color="#146BF6" />}
+        onHomePress={handleGoHome}
+        onMapPress={handleGoMap}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+  scrollContent: {
+    flex: 1,
+  },
+  scrollContentContainer: {
+    paddingVertical: 10,
+  },
+});
