37e60b71401130780ac3d34556a0e1ff635e11b6
floramos06 <ramosg_flor@outlook.com>
2025-10-16 01:18:21 -0400

    Template de superadmin hecho

---
+++ componentes/templates/princSuper.tsx
@@ -0,0 +1,80 @@
+import Boton from "@/componentes/atomos/boton";
+import TituloPestania from "@/componentes/atomos/tituloPestania";
+import CardCierre from "@/componentes/moleculas/cardCierre";
+import HeaderSimple from "@/componentes/moleculas/headerSimple";
+import { Ionicons } from "@expo/vector-icons";
+import React, { memo } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import MenuInf from "../moleculas/menuInf";
+
+const princSuper= () => {
+  //ejm para ver y probar scroll
+  const cierres = [
+    {
+      titulo: "Mel tonta",
+      subtitulo: ["Zona", "Duración: 2 días", "Motivo: Amago de tuberías"],
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <HeaderSimple />
+      <ScrollView contentContainerStyle={styles.content}>
+        <TituloPestania style={styles.title}>Cierres Activos</TituloPestania>
+        
+        {cierres.map((cierre, index) => (
+          <CardCierre
+            key={index}
+            titulo={cierre.titulo}
+            subtitulo={cierre.subtitulo}
+            onPressEditar={() => console.log("Editar cierre", cierre.titulo)}
+            onPressEliminar={() => console.log("Eliminar cierre", cierre.titulo)}
+            style={styles.card}
+          />
+        ))}
+        
+        <Boton
+          texto="Crear nuevo cierre"
+          onPress={() => console.log("Crear nuevo cierre")}
+          variante="primario"
+          tamaño="grande"
+          ancho="completo"
+          estilo={styles.button}
+        />
+      </ScrollView>
+       <MenuInf
+      homeIcon={<Ionicons name="home-outline" size={28} color="#146BF6" />}
+      mapIcon={<Ionicons name="map-outline" size={28} color="#146BF6" />}
+      onHomePress={() => console.log("Home pressed")}
+      onMapPress={() => console.log("Map pressed")}
+    />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F8F8F8",
+    paddingTop: 0,
+    paddingHorizontal: 16,
+  },
+  content: {
+    alignItems: "center",
+    paddingBottom: 50,
+    paddingTop: 20,
+  },
+  title: {
+    marginVertical: 20,
+    marginHorizontal: 6,
+    alignSelf: "flex-start",
+  },
+  card: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 20,
+  },
+});
+
+export default memo(princSuper);
