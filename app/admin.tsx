f612f4abf2c7b34fa60a1d185de0500343a42832
Milena <miletorrico0041@gmail.com>
2025-10-23 15:52:43 -0400

    Pantallas Admin y SuperAd

---
+++ app/admin.tsx
@@ -0,0 +1,7 @@
+import PrincAdmin from '@/componentes/templates/princAdmin';
+
+export default function principalAdmin() {
+  return (
+   <PrincAdmin/>
+  );
+}
---
+++ app/superAdmin.tsx
@@ -0,0 +1,7 @@
+import PrincSuper from '@/componentes/templates/princSuper';
+
+export default function principalSuperAd() {
+  return (
+   <PrincSuper/>
+  );
+}
