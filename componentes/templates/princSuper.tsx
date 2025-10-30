2351ae9f9af57dc66d001227c4c02c6cabf6996e
Milena <miletorrico0041@gmail.com>
2025-10-30 19:56:48 -0400

    Correcciones del merge innecesario xd

--- componentes/templates/princSuper.tsx
+++ componentes/templates/princSuper.tsx
@@ -3,20 +3,13 @@ import TituloPestania from "@/componentes/atomos/tituloPestania";
 import CardCierre from "@/componentes/moleculas/cardCierre";
 import HeaderSimple from "@/componentes/moleculas/headerSimple";
 import MenuInf from "@/componentes/moleculas/menuInf";
+import ModalConfirmacion from "@/componentes/moleculas/modalConfirmacion";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from "expo-constants";
 import { useRouter } from "expo-router";
 import React, { memo, useCallback, useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Alert,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
+import { ActivityIndicator, Alert, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
 
 type Cierre = {
   id: number;
@@ -60,49 +53,8 @@ const obtenerApiUrl = () => {
 
 const API_BASE = obtenerApiUrl();
 
-type Cierre = {
-  id: number;
-  categoria: string | null;
-  lugarCierre: string;
-  idZona: number | null;
-  fechaInicio: string;
-  fechaFin: string;
-  descripcion: string | null;
-  createdAt: string;
-  modifiedAt: string;
-  zona: { id: number; nombreZona: string } | null;
-  ubicaciones: Array<{
-    id: number;
-    idCierre: number;
-    latitud: string;
-    longitud: string;
-  }>;
-};
+const princSuper= () => {
 
-//  Detección automática de IP y fallback
-const obtenerApiUrl = () => {
-  try {
-    const host =
-      Constants?.expoConfig?.hostUri ||
-      Constants?.manifest2?.extra?.expoClient?.hostUri;
-
-    if (host) {
-      const ip = host.split(":")[0];
-      const apiUrl = `http://${ip}:3000/api`;
-      console.log("🌐 API URL detectada automáticamente:", apiUrl);
-      return apiUrl;
-    }
-  } catch (error) {
-    console.warn("⚠️ No se pudo detectar la IP local automáticamente.");
-  }
-
-  console.log("🌐 Usando localhost como fallback");
-  return "http://localhost:3000/api";
-};
-
-const API_BASE = obtenerApiUrl();
-
-const PrincSuper = () => {
   const router = useRouter();
   const [cierres, setCierres] = useState<Cierre[]>([]);
   const [cargando, setCargando] = useState(true);
@@ -357,6 +309,8 @@ const PrincSuper = () => {
           estilo={styles.button}
         />
       </ScrollView>
+
+      {/* Menú Inferior */}
       <MenuInf
         homeIcon={<Ionicons name="home-outline" size={28} color="#146BF6" />}
         usersIcon={<Ionicons name="people-outline" size={28} color="#146BF6" />}
